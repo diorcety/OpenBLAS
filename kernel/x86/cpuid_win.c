c96e326e0da264b92fd3f911eb7f074135a8d863
Yann Diorcet <yann.diorcet@xenocs.com>
2020-11-03 12:12:33 +0100

    Fix compilation on X86 with clang-cl

--- kernel/x86/cpuid_win.c
+++ kernel/x86/cpuid_win.c
@@ -25,7 +25,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************/
 
-#if defined(_MSC_VER) && !defined(__clang__)
+#if defined(_MSC_VER)
 
 #include<intrin.h>
 
